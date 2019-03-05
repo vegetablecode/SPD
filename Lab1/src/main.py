4e2451358ec37613fb2894d7a7f9508d5074bdb5
Kamil <kamil4525@gmail.com>
2019-03-05 10:56:04 +0100

    bruteforce & makespan for any number of machines

--- Lab1/src/main.py
+++ Lab1/src/main.py
@@ -2,25 +2,32 @@ import os
 import sys
 import re
 from src.task import Task
+from src.bruteforce import permute
 
 
-def makespan(order1, order2):
-    time1 = 0
-    time2 = 0
-    for i in range (0, len(order1)):
-        time1 += order1[i]
-        time2 += (time1 + order2[i])
+def makespan(order, tasks, numb_of_machines):
+    times = []
+    for j in range(0, numb_of_machines):
+        times.append(0)
+    for i in order:
+        i -= 1
+        times[0] += tasks[i].times[0]
+        for j in range(1, numb_of_machines):
+            if times[j] < times[j-1]:
+                times[j] = times[j-1]
+            times[j] += tasks[i].times[j]
 
+    return max(times)
 
 
 # open file with data
 file_dir = os.path.dirname(os.path.realpath('__file__'))
 filename = os.path.join(file_dir, '../datasets/data.1')
 
+# if the file does not exist
 if not os.path.isfile(filename):
     print("File path {} does not exist. Exiting...".format(filename))
     sys.exit()
-
 
 # convert file to list of tasks
 tasks = []
@@ -35,11 +42,20 @@ with open(filename) as fp:
             tasks.append(Task(n))
         cnt += 1
 
-order1 = []
-order2 = []
-for task in tasks:
-    order1.append(task.times[0])
-    order2.append(task.times[1])
-    print(task.times)
+# generating index table
+index_table = []
+for i in range(1, numb_of_items+1):
+    index_table.append(i)
 
-makespan(order1, order2)
+
+# searching for min makespan with bruteforce
+best_order = index_table
+best_makespan = makespan(best_order, tasks, numb_of_machines)
+
+for p in permute(index_table):
+    if makespan(p, tasks, numb_of_machines) < best_makespan:
+        best_order = list(p)
+        best_makespan = makespan(p, tasks, numb_of_machines)
+
+print("Best order: {}" .format(best_order))
+print("Best makespan: {}" .format(best_makespan))
