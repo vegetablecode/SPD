7752e2024ed80a02dc5f230c1a69c5d1b3f8661a
Kamil <kamil4525@gmail.com>
2019-03-17 17:06:59 +0100

    import bugfix

--- Lab1/src/main.py
+++ Lab1/src/main.py
@@ -1,29 +1,28 @@
 import copy
-from datareader import get_data
-from bruteforce import bruteforce
-from johnson import johnson
-from makespan import makespan, get_order
-from gantt import draw_gantt
-from generating import generating
+from src.datareader import get_data
+from src.bruteforce import bruteforce
+from src.johnson import johnson
+from src.makespan import makespan, get_order
+from src.gantt import draw_gantt
+from src.generating import generating
+
 
 def ta000Instance(choice):
-    #Loading a ta000 instance
+    #  Loading a ta000 instance
     if choice == 1:
         tasks, numb_of_machines = get_data("ta000_2")
     elif choice == 2:
         tasks, numb_of_machines = get_data("ta000_3")
 
-
-    # searching for min makespan with Bruteforce
+    #  searching for min makespan with Bruteforce
     bruteforce_order, bruteforce_time = bruteforce(copy.deepcopy(tasks), numb_of_machines)
     bruteforce_makespan = makespan(bruteforce_order, tasks, numb_of_machines)
 
-    # searching for min makespan with Johnson
-    johnson_order, johnson_time = johnson(copy.deepcopy(tasks),numb_of_machines)
+    #  searching for min makespan with Johnson
+    johnson_order, johnson_time = johnson(copy.deepcopy(tasks), numb_of_machines)
     johnson_makespan = makespan(johnson_order, tasks, numb_of_machines)
 
-
-    # descriptions
+    #  descriptions
     print("---------------------------")
     print("Best order (Bruteforce): {}" .format(bruteforce_order))
     print("Best makespan (Bruteforce): {}" .format(bruteforce_makespan))
@@ -31,15 +30,15 @@ def ta000Instance(choice):
     print("Best order (Johnson): {}" .format(johnson_order))
     print("Best makespan (Johnson): {}" .format(johnson_makespan))
 
-
-    # plot figures
+    #  plot figures
     plt = draw_gantt(bruteforce_order, tasks, numb_of_machines, bruteforce_time, "Podzial zadan (przeglad zupelny)")
     plt = draw_gantt(johnson_order, tasks, numb_of_machines, johnson_time, "Podzial zadan (algorytm Johnsona)")
     plt = draw_gantt(get_order(tasks), tasks, numb_of_machines, 0, "Podzial zadan (naturalna kolejnosc)")
     plt.show()
 
-#MAIN INTERFACE DEFINED
-print ("")
+
+#  MAIN INTERFACE DEFINED
+print("")
 print("BRUTEFORCE and JOHNSON algorithm program")
 print("----------------------------------------")
 print("1 - Dzialanie na podstawie ta000 2-maszynowego")
@@ -52,10 +51,10 @@ choice = int(input("Enter a number: "))
 if choice == 1 or choice == 2:
     ta000Instance(choice)
 elif choice ==3:
-    #Generating function
+    #  Generating function
     a = int(input("Ile tablic wygenerowac? "))
     generating(a)
-    #print("")
+    #  print("")
 elif choice == 4:
     exit()
-#----------------------
+#  ----------------------
