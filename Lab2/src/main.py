36e0de7ae7ecdfce03043d25d18ad3d0a7f277cf
Adam <adamkrzykala97@gmail.com>
2019-03-24 21:27:04 +0100

    IR4 modyfication added

--- Lab2/src/main.py
+++ Lab2/src/main.py
@@ -3,14 +3,14 @@ from datareader import get_data
 from gantt import draw_gantt
 from neh import neh
 
-tasks, numb_of_machines = get_data("data.1")
+tasks, numb_of_machines = get_data("ta040")
 
 # searching for min makespan with NEH
-neh_order, neh_time = neh(copy.deepcopy(tasks), numb_of_machines,0)
-print ("")
-neh_order, neh_time = neh(copy.deepcopy(tasks), numb_of_machines,1)
-print ("")
-neh_order, neh_time = neh(copy.deepcopy(tasks), numb_of_machines,2)
+#neh_order, neh_time = neh(copy.deepcopy(tasks), numb_of_machines,0)
+#print ("")
+#neh_order, neh_time = neh(copy.deepcopy(tasks), numb_of_machines,1)
+#print ("")
+neh_order, neh_time = neh(copy.deepcopy(tasks), numb_of_machines,4)
 
 #plt = draw_gantt(neh_order, tasks, numb_of_machines, neh_time, "Podzial zadan")
 #plt.show()
--- Lab2/src/neh.py
+++ Lab2/src/neh.py
@@ -50,14 +50,33 @@ def nehIR2(tasks_cp,solution_order_cp,lastIndexElement,numb_of_machines):
             lowest_makespan_sequence = sequence
     return lowest_makespan_sequence
 
+
 #Defining IR3 modyfication
 def nehIR3(void):
     print("cos")
 
 #Defining IR4 modyfication
-def nehIR4(void):
-    print("cos")
-
+def nehIR4(tasks_cp,solution_order_cp,lastIndexElement,numb_of_machines):
+    taskNumber = 0
+    diffMax = 0
+    makespanBase = makespan(solution_order_cp,tasks_cp,numb_of_machines)
+    for index,task in enumerate(tasks_cp):
+        if index in solution_order_cp:
+            idx = solution_order_cp.index(index)
+            solution_order_cp.remove(index)
+            makespanTemp = makespan(solution_order_cp,tasks_cp,numb_of_machines)
+            diffTemp = makespanBase - makespanTemp
+            if diffTemp > diffMax: taskNumber = index
+            solution_order_cp.insert(idx,index)
+    if lastIndexElement == taskNumber: return solution_order_cp
+    lowest_makespan = float("inf")
+    lowest_makespan_sequence = []
+    sequences = get_sequences(taskNumber,solution_order_cp)
+    for sequence in sequences:
+        if makespan(sequence, tasks_cp, numb_of_machines) < lowest_makespan:
+            lowest_makespan = makespan(sequence, tasks_cp, numb_of_machines)
+            lowest_makespan_sequence = sequence
+    return lowest_makespan_sequence
 
 
 def neh(tasks, numb_of_machines,neh_type):
@@ -74,7 +93,6 @@ def neh(tasks, numb_of_machines,neh_type):
     solution_order = []
     for i in omegas_order:  # (3) get task with the highest omega value
         # (4) insert task & pick task with the lowest makespan
-        print i
         lowest_makespan = float("inf")
         lowest_makespan_sequence = []
         sequences = get_sequences(i, solution_order)
@@ -97,6 +115,7 @@ def neh(tasks, numb_of_machines,neh_type):
             nehIR3()
         if neh_type == 4:
             #IR4
-            nehIR4()
+            solution_order = nehIR4(tasks,solution_order,i,numb_of_machines)
+    print lowest_makespan
     stop = timer()
     return solution_order, (stop-start)*1000
