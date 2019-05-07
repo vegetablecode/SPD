3987585d4bb0ba11b9177e637b63ac40114dd284
Kamil <kamil4525@gmail.com>
2019-05-07 23:52:46 +0200

    create schrage o(n^2)

--- Lab4/src/main.py
+++ Lab4/src/main.py
@@ -1,14 +1,20 @@
 import copy
 from src.datareader import get_data
 from src.makespan import makespan, to_natural_order, get_order
+from src.schrage_n2 import schrage_n2
 
-tasks = get_data("rpq_500.txt")  # cmax -> 5.446
+tasks = get_data("data.004")
 
 # print all tasks
-for task in tasks:
-    print(task.index, " ", task.times)
+#for task in tasks:
+#    print(task.index, " ", task.times)
 
 # INITIAL ORDER
 init_order = get_order(tasks)
 init_makespan = makespan(init_order, tasks)
 print("[INIT] makespan: ", init_makespan)
+
+# SCHRAGE ORDER
+schrage_n2_order = schrage_n2(tasks)
+shrage_n2_makespan = makespan(schrage_n2_order, tasks)
+print("[SHRAGE N^2] makespan: ", shrage_n2_makespan)
