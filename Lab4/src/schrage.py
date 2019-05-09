53228692e732fbbd99ff8e35b9e2ecb045d47023
Kamil <kamil4525@gmail.com>
2019-05-09 16:56:53 +0200

    add schrage pmtn (almost working)

--- Lab4/src/schrage.py
+++ Lab4/src/schrage.py
@@ -1,6 +1,7 @@
 from src.makespan import get_order, makespan
 import copy
 import numpy as np
+from src.task import Task
 
 def get_column(tasks, element):
     column = []
@@ -38,23 +39,23 @@ def schrage_n2(tasks):
 
 
 def schrage_n2_pmtn(tasks):
+    W_tasks = []  # temporary order
     G_tasks = []  # ready to order tasks
     N_tasks = copy.deepcopy(tasks)
     t = get_min(get_column(N_tasks, 0))
-    l = 0
-    cmax = 0
     q_0 = 99999999
+    task_l = Task(0, [0, 0, q_0])  # current task
+    cmax = 0
 
     while len(N_tasks) != 0 or len(G_tasks) != 0:
         while len(N_tasks) != 0 and get_min(get_column(N_tasks, 0)) <= t:
             j = np.argmin(get_column(N_tasks, 0))
             G_tasks.append(N_tasks[j])
-            task_j = copy.deepcopy(tasks[j])
-            task_l = copy.deepcopy(tasks[l])
+            task_j = copy.deepcopy(N_tasks[j])
             del N_tasks[j]
             if task_j.times[2] > task_l.times[2]:
-                task_l.times[1] = t - N_tasks[j].times[0]
-                t = N_tasks[j].times[0]
+                task_l.times[1] = t - task_j.times[0]
+                t = task_j.times[0]
                 if task_l.times[1] > 0:
                     G_tasks.append(task_l)
         if len(G_tasks) == 0:
@@ -62,6 +63,8 @@ def schrage_n2_pmtn(tasks):
         else:
             j = np.argmax(get_column(G_tasks, 2))
             t += G_tasks[j].times[1]
+            W_tasks.append(G_tasks[j])
+            task_l = task_j
+            cmax = max(cmax, t + task_j.times[2])
             del G_tasks[j]
-            cmax = max(cmax, t + N_tasks[j].times[2])
     return cmax
