fc78ca66bf4be32b925582c5f562a513f83fb4e9
Kamil <kamil4525@gmail.com>
2019-05-09 23:43:31 +0200

    add extended potts algorithm (partial)

--- Lab4/src/extended_potts.py
+++ Lab4/src/extended_potts.py
@@ -1,17 +1,20 @@
 import copy
 import numpy as np
 from src.schrage import get_column, get_min
-from src.makespan import get_order
+
 
 # Implementation of NI-P algorithm
 # https://www.researchgate.net/publication/241080533_Approximation_algorithms_for_no_idle_time_scheduling_on_a_single_machine_with_release_times_and_delivery_times
 # Page: 5
+
 
 def nip_schrage(tasks):
     W_tasks = []  # temporary order
     G_tasks = []  # ready to order tasks
     N_tasks = copy.deepcopy(tasks)
     t = get_min(get_column(N_tasks, 0))
+    cmax = 0
+    critical_task = 0
 
     while len(N_tasks) != 0 or len(G_tasks) != 0:
         while len(N_tasks) != 0 and get_min(get_column(N_tasks, 0)) <= t:
@@ -22,7 +25,28 @@ def nip_schrage(tasks):
             t = get_min(get_column(N_tasks, 0))
         else:
             j = np.argmax(get_column(G_tasks, 2))
-            t += G_tasks[j].times[1]
-            W_tasks.append(G_tasks[j])
+            tasks_j = copy.deepcopy(G_tasks[j])
             del G_tasks[j]
-    return get_order(W_tasks)
+            t += tasks_j.times[1]
+            W_tasks.append(tasks_j)
+            if cmax < t + tasks_j.times[2]:
+                cmax = t + tasks_j.times[2]
+                critical_task = tasks_j
+
+    g = np.argmin(get_column(W_tasks, 2))
+    task_min_q = copy.deepcopy(W_tasks[g])
+
+    # if critical task has the shortest q -> interference task does not exist
+    if task_min_q == critical_task.times[2]:
+        interference_task_index = -1
+    else:
+        interference_task = W_tasks[0]
+        for i in range(W_tasks.index(critical_task)+1):
+            for j in range(W_tasks.index(interference_task)+1, W_tasks.index(critical_task)+1):
+                if W_tasks[j].times[2] < critical_task.times[2]:
+                    interference_task = W_tasks[j]
+                    break
+        interference_task_index = W_tasks.index(interference_task)
+
+    return cmax, W_tasks, W_tasks.index(critical_task), interference_task_index
+
--- Lab4/src/main.py
+++ Lab4/src/main.py
@@ -3,6 +3,7 @@ from src.datareader import get_data
 from src.makespan import makespan, to_natural_order, get_order
 from src.schrage import schrage_n2, schrage_n2_pmtn
 from src.simulated_annealing import simulated_annealing
+from src.extended_potts import nip_schrage
 
 tasks = get_data("data.001")
 
@@ -20,6 +21,9 @@ print("[SHRAGE N^2] makespan: ", shrage_n2_makespan)
 schrage_n2_ptmn_makespan, schrage_n2_ptmn_order = schrage_n2_pmtn(tasks)
 print("[SHRAGE N^2 PMTN] makespan:", schrage_n2_ptmn_makespan)
 
-sa_order = simulated_annealing(copy.deepcopy(tasks), 500000, 0.0001, 0.999)
-sa_makespan = makespan(sa_order, tasks)
-print("[SA] makespan:", sa_makespan)
+#sa_order = simulated_annealing(copy.deepcopy(tasks), 500000, 0.0001, 0.999)
+#sa_makespan = makespan(sa_order, tasks)
+#print("[SA] makespan:", sa_makespan)
+
+cmax, ord, d, f = nip_schrage(tasks)
+print("[NIP SHRAGE N^2] makespan: ", cmax)
