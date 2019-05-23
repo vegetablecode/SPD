69e29ff2083c7690ad9b90954ce4d85a1cb26450
Kamil <kamil4525@gmail.com>
2019-05-23 20:24:22 +0200

    add greedy algorithm

--- Lab5/src/carlier.py
+++ Lab5/src/carlier.py
@@ -2,6 +2,7 @@ from src.schrage import schrage, schrage_pmtn
 import copy
 from timeit import default_timer as timer
 from src.datareader import get_data
+import numpy as np
 
 
 def calculate_b(u, pi):
@@ -111,6 +112,111 @@ def carlier_dl(tasks):
     return ub
 
 
+def sort_tasks(pi_list, lb_list):
+    order = np.argsort(lb_list)
+    new_pi_list = []
+    new_lb_list = []
+    for i in order:
+        new_pi_list.append(pi_list[i])
+        new_lb_list = [lb_list[i]]
+    pi_list = copy.deepcopy(new_pi_list)
+    lb_list = copy.deepcopy(new_lb_list)
+
+
+# wide left
+def carlier_greedy(tasks):
+    # ----- "GLOBAL" VARIABLES DECLARATION ----- #
+    # u: cmax for schrage
+    # pi: tasks sorted with schrage
+    # ub: upper border
+    # lb: lower border
+
+    global u
+    global pi
+    global ub
+    global lb
+    global pi_list
+    global lb_list
+
+    u, pi = schrage(copy.deepcopy(pi_list[0]))
+    if u < ub:
+        ub = u
+
+    # find b, a, c values
+    b = calculate_b(u, pi)
+    a = calculate_a(b, u, pi)
+    c = calculate_c(b, a, pi)
+
+    #  if c doesn't exist
+    if c < 0:
+        return ub
+
+    # calculate r1, p1, q1
+    r_1 = pi[c + 1].times[0]
+    p_1 = 0
+    q_1 = pi[c + 1].times[2]
+
+    for i in range(c + 1, b + 1):
+        p_1 += pi[i].times[1]
+        if r_1 > pi[i].times[0]:
+            r_1 = pi[i].times[0]
+        if q_1 > pi[i].times[2]:
+            q_1 = pi[i].times[2]
+
+    # LEFT NODE
+    r_pi_c = pi[c].times[0]
+    pi[c].times[0] = max(pi[c].times[0], r_1 + p_1)
+
+    p_2 = 0
+    r_2 = pi[c].times[0]
+    q_2 = pi[c].times[2]
+
+    for i in range(c, b + 1):
+        p_2 += pi[i].times[1]
+        if r_2 > pi[i].times[0]:
+            r_2 = pi[i].times[0]
+        if q_2 > pi[i].times[2]:
+            q_2 = pi[i].times[2]
+
+    lb, ptmn_order = schrage_pmtn(copy.deepcopy(pi))
+    lb = max(max(r_1 + q_1 + p_1, r_2 + q_2 + p_2), lb)
+    if lb < ub:
+        pi_list.append(copy.deepcopy(pi))
+        lb_list.append(lb)
+    pi[c].times[0] = r_pi_c
+
+    # RIGHT NODE
+    r_pi_c = pi[c].times[2]
+    pi[c].times[2] = max(pi[c].times[2], q_1 + p_1)
+
+    p_2 = 0
+    r_2 = pi[c].times[0]
+    q_2 = pi[c].times[2]
+
+    for i in range(c, b + 1):
+        p_2 += pi[i].times[1]
+        if r_2 > pi[i].times[0]:
+            r_2 = pi[i].times[0]
+        if q_2 > pi[i].times[2]:
+            q_2 = pi[i].times[2]
+
+    lb, order = schrage_pmtn(copy.deepcopy(pi))
+    lb = max(max(r_1 + q_1 + p_1, r_2 + q_2 + p_2), lb)
+
+    if lb < ub:
+        pi_list.append(copy.deepcopy(pi))
+        lb_list.append(lb)
+    pi[c].times[2] = r_pi_c
+
+    # remove checked node from the list
+    pi_list.pop(0)
+    lb_list.pop(0)
+
+    # sort tasks
+    sort_tasks(pi_list, lb_list)
+    return carlier_greedy(tasks)
+
+
 # wide left
 def carlier_wl(tasks):
     # ----- "GLOBAL" VARIABLES DECLARATION ----- #
@@ -208,7 +314,7 @@ for i in range(0, len(task_list)):
     print("TEST: ", task_list[i])
     print("-")
 
-    # ---------------- DEEP LEFT
+    # ------------------------------------------------ DEEP LEFT
     ub = 999999999
     u, pi = schrage(copy.deepcopy(tasks))
 
@@ -224,7 +330,7 @@ for i in range(0, len(task_list)):
     print("RESULT: ", test_result, " | SHOULD BE: ", result_list[i])
     print("-")
 
-    # ---------------- WIDE LEFT
+    # ------------------------------------------------ WIDE LEFT
     u, pi = schrage(copy.deepcopy(tasks))
     ub = 999999999
     lb = 0
@@ -241,4 +347,25 @@ for i in range(0, len(task_list)):
     # VALIDATION
     test_result = ["BAD RESULT", "OK"][ub == result]
     print("RESULT: ", test_result, " | SHOULD BE: ", result_list[i])
+    print("-")
+
+    # ------------------------------------------------ WIDE GREEDY
+    u, pi = schrage(copy.deepcopy(tasks))
+    ub = 999999999
+    lb = 0
+
+    pi_list = [pi]
+    lb_list = [lb]
+
+    # ALGORITHM
+    start = timer()
+    carlier_greedy(copy.deepcopy(tasks))
+    stop = timer()
+    carlier_time = (stop - start) * 1000
+    print("[CARLIER GREEDY] makespan: {}, time: {}".format(ub, carlier_time))
+
+    # VALIDATION
+    test_result = ["BAD RESULT", "OK"][ub == result]
+    print("RESULT: ", test_result, " | SHOULD BE: ", result_list[i])
+
     print("---------------------------------------------")
